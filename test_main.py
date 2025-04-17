187f1279bf91105f72fd1e75ef38200a1839df67
Your Name <you@example.com>
2025-04-17 15:24:37 +0400

    fixed flake 8 errors

--- test_main.py
+++ test_main.py
@@ -1,13 +1,17 @@
 from main import add
 
+
 def test_add():
     assert add(2, 3) == 5
+
 
 def test_add_negative():
     assert add(-1, -4) == 5
 
+
 def test_add_zero():
     assert add(0, 5) == -5
+
 
 def test_add_one():
     assert add(0, 1) == 1

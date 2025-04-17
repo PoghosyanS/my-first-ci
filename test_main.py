5af9910fbc4dfeed45157b5356d44b4f1a60014d
Your Name <you@example.com>
2025-04-17 15:31:33 +0400

    fixed test_main.py file

--- test_main.py
+++ test_main.py
@@ -6,11 +6,11 @@ def test_add():
 
 
 def test_add_negative():
-    assert add(-1, -4) == 5
+    assert add(-1, -4) == -5
 
 
 def test_add_zero():
-    assert add(0, 5) == -5
+    assert add(0, 5) == 5
 
 
 def test_add_one():

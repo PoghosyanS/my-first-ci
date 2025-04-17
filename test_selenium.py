09e833e1894f4b22579af11d9fc43905ce9d8917
Your Name <you@example.com>
2025-04-17 17:59:17 +0400

    k

--- test_selenium.py
+++ test_selenium.py
@@ -3,7 +3,7 @@ from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 
 options = Options()
-options.headless = True  # Отключает открытие браузера на экране (необязательно)
+options.headless = True  # Отключает открытие браузера на экране 
 
 service = Service()  # Geckodriver автоматически используется из PATH
 driver = webdriver.Firefox(service=service, options=options)
